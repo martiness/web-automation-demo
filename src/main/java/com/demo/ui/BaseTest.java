386a3611e74e059e6c21bbaa4f1a7f1cfd2def94
martin.kenov <martin.kenov@gmail.com>
2025-05-19 22:38:26 +0300

    Adding support of Chrome and Firefox browsers

--- src/main/java/com/demo/ui/BaseTest.java
+++ src/main/java/com/demo/ui/BaseTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -20,25 +22,36 @@ public abstract class BaseTest {
 
     @BeforeEach
     public void setUp() {
-        ChromeOptions options = new ChromeOptions();
+        String browser = System.getProperty("browser", "chrome").toLowerCase();
 
-        // Skip Google Password pop-up
-        options.addArguments("--incognito");
-        options.addArguments("--disable-notifications");
-        options.addArguments("--disable-popup-blocking");
-        options.addArguments("--disable-save-password-bubble");
-        options.addArguments("disable-infobars");
-        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+        switch (browser) {
+            case "firefox":
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("-private");
+                firefoxOptions.addPreference("dom.webnotifications.enabled", false);
+                firefoxOptions.addPreference("dom.disable_open_during_load", false);
+                firefoxOptions.addPreference("signon.rememberSignons", false);
+                driver = new FirefoxDriver(firefoxOptions);
+                break;
+            case "chrome":
+            default:
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--incognito");
+                options.addArguments("--disable-notifications");
+                options.addArguments("--disable-popup-blocking");
+                options.addArguments("--disable-save-password-bubble");
+                options.addArguments("disable-infobars");
+                options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+                driver = new ChromeDriver(options);
+                break;
+        }
 
-        driver = new ChromeDriver(options);
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
 
         baseUrl = ConfigReader.get("base.url");
         driver.get(baseUrl);
-
         wait.until(ExpectedConditions.titleContains("Swag Labs"));
     }
 
